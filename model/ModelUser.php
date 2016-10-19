6b1b2ad8f5a20bd5b8b0ad0c81b0581ad75346ef
kizardjianl <kizardjianl@K133-26.dep-info.iutmontp.univ-montp2.fr>
2016-10-19 18:21:19 +0200

    model.php suru

--- model/ModelUser.php
+++ model/ModelUser.php
@@ -1,8 +1,59 @@
 <?php
 
-class User {
+require_once File::build_path(array('model','Model.php'));
+
+class ModelUser {
     
+    private $id;
+    private $username;
+    private $userfirstname;
+    private $mail;
+    private $birthdate;
     
+    public function __construct($id = NULL, $userN = NULL, $userFN = NULL, $mail = NULL, $bd = NULL){
+        if(!is_null($id) && !is_null($userN) && !is_null($userFN) &&!is_null($mail) && !is_null($bd)){
+            $this->id = $id;
+            $this->username = $userN;
+            $this->userfirstname = $userFN;
+            $this->mail = $mail;
+            $this->birthdate = $bd;
+        }
+    }
+    
+    public static function displayById($id){
+        
+        $query = "SELECT * 
+                  FROM Users
+                  WHERE idUser = :idUs";
+        
+        try{
+            $prepared = Model::$pdo->prepare($sql);
+            $values = array (
+                'idUs'=>$id,
+            );
+            $req_prep->execute($values);
+            $req_prep->setFetchMode(PDO::FETCH_CLASS,'ModelUser');
+            return $req_prep->fetch();
+        } catch (PDOException $ex) {
+            if(Conf::getDebug()){
+                echo $ex->getMessage();
+            }
+            else{
+                echo "une erreur est survenue.";
+            }
+        }    
+    }
+    
+    public static function displayAll(){
+    
+        $query = "SELECT * FROM Users";
+        try{
+            
+        } catch (Exception $ex) {
+
+        }
+        
+    }
     
 }
 
