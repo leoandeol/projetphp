4b2510ea4a3251e2c6253f8e0ab01d63f4e502cb
kizardjianl <kizardjianl@K133-26.dep-info.iutmontp.univ-montp2.fr>
2016-10-19 17:39:22 +0200

    j'ai fait le config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,44 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Conf {
 
+    static private $debug = true;
+    static private $databases = array(
+        // Le nom d'hote est infolimon a l'IUT
+        // ou localhost sur votre machine
+        'hostname' => 'infolimon.iutmontp.univ-montp2.fr',
+        // A l'IUT, vous avez une BDD nommee comme votre login
+        // Sur votre machine, vous devrez creer une BDD
+        'database_name' => 'kizardjianl',
+        // A l'IUT, c'est votre login
+        // Sur votre machine, vous avez surement un compte 'root'
+        'login' => 'kizardjianl',
+        // A l'IUT, c'est votre mdp (INE par defaut)
+        // Sur votre machine personelle, vous avez creez ce mdp a l'installation
+        'password' => '0207030504G'
+    );
+
+    static public function getDebug() {
+        return self::$debug;
+    }
+
+    static public function getLogin() {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['login'];
+    }
+
+    static public function getHostName() {
+        return self::$databases['hostname'];
+    }
+
+    static public function getDatabaseName() {
+        return self::$databases['database_name'];
+    }
+
+    static public function getPassword() {
+        return self::$databases['password'];
+    }
+
+}
+
+?>
