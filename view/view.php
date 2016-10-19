6422b678399b9a3aadcbfae0819826a41a573ccf
kizardjianl <kizardjianl@K133-26.dep-info.iutmontp.univ-montp2.fr>
2016-10-19 17:53:25 +0200

    model.php suru

--- view/view.php
+++ view/view.php
@@ -1,8 +1,12 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $pagetitle ?></title>
+    </head>
+    <?php
+    ?>
+    <footer>
+        <p> Jesus INC.</p>  
+    </footer>
+</html>
