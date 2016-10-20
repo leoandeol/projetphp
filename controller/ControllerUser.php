48599869962213ac20e1dd67cbb404a7f65abaa5
LK <lothair.kizardjian@laposte.net>
2016-10-20 14:19:39 +0200

    Modif de controller User, du routeur

--- controller/ControllerUser.php
+++ controller/ControllerUser.php
@@ -7,8 +7,16 @@ class ControllerUser{
     public function read(){        
         $id = $_POST['idUser'];
         $tab_user = ModelUser::getUserById($id);
-        $view = 'readUser.php';
-        $controller = 'view';
+        $view = 'displayUser.php';
+        $controller = 'user';
+        $pagetitle = 'Description';
+        File::build_path(array('view','view.php'));
+    }
+    
+    public function readAll(){
+        $tab_user = ModelUser::getAllUser();
+        $view = 'displayUser.php';
+        $controller = 'user';
         $pagetitle = 'Description';
         File::build_path(array('view','view.php'));
     }
--- controller/routeur.php
+++ controller/routeur.php
@@ -1,8 +1,26 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if(!isset($_GET['action'])){
+    $action = 'readAllUser';
+}else{
+    $action = $_GET['action'];
+}
+if(!isset($_GET['controller'])){
+    $controller = 'Utilisateur';
+}else{
+    $controller = $_GET['controller'];
+}
+
+$controllerClass = 'Controller' . ucfirst($controller);
+
+if(!(class_exists($controllerClass))){
+    ControllerUser::error(); // error() to do
+}else{
+    if(!(in_array($action,  get_class_methods($controllerClass)))){
+        ControllerUser::error();
+    }else{
+        $controllerClass::$action;
+    }    
+}
+?>
 
