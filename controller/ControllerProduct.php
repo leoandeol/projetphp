8f1c6d6b23578480b3037a9ecf1ca277a87575d0
govinc <govinc@K133-1.dep-info.iutmontp.univ-montp2.fr>
2016-10-25 16:42:25 +0200

    Rajout dossier view product+ view formulaire ajout produit + controllerproduit + maj modelProduit

---
+++ controller/ControllerProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once File::build_path(array('model','ModelUser.php'));
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ControllerProduct
+ *
+ * @author govinc
+ */
+class ControllerProduct {
+    
+    function read(){
+        
+    }
+    
+}
--- model/ModelProduct.php
+++ model/ModelProduct.php
@@ -1,8 +1,82 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once FILE::build_path(array('model', 'Model.php'));
+
+class Product{
+    
+    private $idProduct;
+    private $productName;
+    private $price;
+    
+    private static $compteur = 0;
+    
+ // ###################################################      
+    // Getters
+    public function getIdProduct(){
+        return $this->idProduct;
+    }
+    public function getProductName(){
+        return $this->productName;
+    }
+    public function getPrice(){
+        return $this->price;
+    }
+    
+    
+ // ###################################################      
+    // Setters  
+    public function setIdProduct($id){
+        $this->idProduct = $id;
+    }
+    public function setProductName($bird){
+        $this->productName = $bird;
+    }
+    public function setPrice($bird){
+        $this->price = $bird;
+    }
+    
+// ###################################################      
+    // Fonction    
+    public function __construc($id = NULL, $pn = NULL, $p = NULL){
+        if (!is_null($id) && !is_null($pn) && !is_null($p)){
+            $this->idProduction = $id;
+            $this->productName = $pn;
+            $this->price = $p;
+        }
+    }
+    
+    public function save(){
+        try{
+            $sql = "INSERT INTO Products(idProduit, productName, price) VALUES(:id, :n, :p";
+            
+            //préparation de la requête
+            $req_prep = Model::$pdo->prepare($sql);
+            
+            $values = array(
+                "id" => self::compteur,
+                "n" => $this->productName,
+                "p" => $this->price
+            );
+            
+            return $req_prep->execute($values);
+            
+        } catch (PDOException $e) {
+            if($e->getCode()==23000){
+                echo '<br>Le produit existe déjà. ';
+                return false;
+            }
+            else{
+                echo '<br>Une erreur est survenue lors de la sauvegarde du produit. ';
+            }
+                
+
+        }
+    }
+    
+  
+    
+}
+
+
+?>
 
