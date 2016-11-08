f9d82f135850819b990018da59dd8ae1ad4808a2
Leo Andeol <leo.andeol@gmail.com>
2016-11-08 18:01:24 +0100

    Changes on register

--- view/user/register.php
+++ view/user/register.php
@@ -3,6 +3,7 @@
     <fieldset>
         <legend>S'inscrire :</legend>
         <input type='hidden' name='action' value='registered'>
+        <input type='hidden' name='controller' value='user'>
         <p>
             <label for="nick_id">Pseudonyme</label> :
             <input type="text" placeholder="Pseudonyme" name="nickname" id="nick_id" required/>
