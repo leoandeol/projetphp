6e1035d6c8c697552e28f07ae39390e64f703ac4
LK <lothair.kizardjian@laposte.net>
2016-12-05 12:21:24 +0100

    various updates

--- lib/Session.php
+++ lib/Session.php
@@ -13,16 +13,10 @@ class Session{
     public static function setLogin($login){
         $_SESSION['login'] = $login;
     }
-    
-<<<<<<< HEAD
-    public static function connect($nickName,$firstName,$lastName,$birthDate,$mail) {
-        $_SESSION['connected'] = true;
-        $_SESSION['nickName'] = $nickName;
-=======
+
     public static function connect($nickname, $firstName,$lastName,$birthDate,$mail) {
         $_SESSION['connected'] = true;
         $_SESSION['nickName'] = $nickname;
->>>>>>> be3c135733c94e4f45332d000c02b88831af9fdf
         $_SESSION['firstName'] = $firstName;
         $_SESSION['lastName']  = $lastName;
         $_SESSION['birthDate'] = $birthDate;
