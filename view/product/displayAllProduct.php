71b22914e33cff3c4e11bf720b51d78c616146c5
Leo Andeol <leo.andeol@gmail.com>
2016-12-12 19:40:55 +0100

    Changing css and html related to product listing

--- view/product/displayAllProduct.php
+++ view/product/displayAllProduct.php
@@ -14,23 +14,21 @@ if ($tab_p != false) {
         $securePPrice = rawurldecode($p->getPrice());
         echo <<< EOT
             <div class="product">
-                <div class="product-name-pic">
-                    <div class="product-name">$pLabel </div>
                     <img class="product-pic" src="res/upload/produit$pId.jpg" />
-                </div>
-                <div class="price">Prix :  $pPrice €</div>
-                <div class="description">$pSDesc</div>
+                    <div class="product-text">$pLabel </div>
+                <div class="product-text">$pPrice €</div>
+                <div class="product-text">$pSDesc</div>
                 
-                <a href="index.php?controller=product&action=read&label=$securePLabel"><div class="detail">Détails</div></a>
-                <a href="index.php?controller=product&action=addPanier&label=$securePLabel&price=$securePPrice"><div class="detail">Ajouter au panier</div></a>
+                <div class="product-text"><a href="index.php?controller=product&action=read&label=$securePLabel">Détails</a></div>
+                <div class="product-text"><a href="index.php?controller=product&action=addPanier&label=$securePLabel&price=$securePPrice">Ajouter au panier</a></div>
 EOT;
-        if(Session::is_admin() && Session::is_connected()){
-         echo <<<EOT
+        if (Session::is_admin() && Session::is_connected()) {
+            echo <<<EOT
             <a href="index.php?controller=product&action=update&label=$securePLabel"><div class="detail">Modifier</div></a>
                 <a href="index.php?controller=product&action=delete&idProduct=$secureId"><div class="detail">Supprimer</div></a>
 EOT;
         }
-                        echo "</div>";
+        echo "</div>";
     }
     echo "</div>";
 }
