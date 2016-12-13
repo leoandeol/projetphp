b691f4bae1140b70f559c3d5aa9d6bc4379d8380
Leo Andeol <leo.andeol@gmail.com>
2016-12-13 13:52:14 +0100

    Merging

--- view/product/displayProduct.php
+++ view/product/displayProduct.php
@@ -28,13 +28,7 @@ if ($nbOption != 0) {
         $pPriceO = htmlspecialchars($object->getPrice());
         $pDescO = htmlspecialchars($object->getDescription());
         
-<<<<<<< HEAD
-        echo <<<EOT
-            <fieldset>
-EOT;
-=======
         echo "<fieldset>";
->>>>>>> 897a2c23fd6d37c36728ec3275acd08b1d5a1cfe
         $name = "check" . $i;
         $i++;
         
